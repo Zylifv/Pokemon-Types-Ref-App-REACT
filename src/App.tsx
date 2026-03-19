0c2592ca2ba8ae7ec2e1423eac246480219d39c1
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-03-19 08:44:24 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,252 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import { typesRef } from "./pokemon";
+import { colorRef } from "./colors";
+
+export default function App() {
+  const typeNames = typesRef.map((name) => name.type);
+  const [search, searchVal] = useState(typeNames);
+
+  const handleChange = (event: any) => {
+    let count = event.target.value.length;
+    if (event.target.value.length > 0) {
+      searchVal(
+        typeNames.filter(
+          (x) =>
+            x.substring(0, count).toLowerCase() ===
+            event.target.value.substring(0, count).toLowerCase()
+        )
+      );
+    } else {
+      searchVal(typeNames);
+    }
+  };
+
+  const displayTypes = typesRef
+    .filter((e) => search.includes(e.type))
+    .map((type, idx) => (
+      <div
+        key={type.type}
+        id={type.type}
+        className="type"
+        style={{
+          borderRadius: "15px",
+          boxShadow: `${type.color} 0px 4px 10px`,
+        }}
+      >
+        <h2>{type.type} type</h2>
+        <img
+          src={type.svg}
+          className="icon"
+          style={{
+            background: `${type.color}`,
+            boxShadow: `0 0 20px ${type.color}`,
+          }}
+        />
+
+        <div
+          className="offense-info"
+          style={{ border: `2px solid ${type.color}` }}
+        >
+          <div
+            className="info-border"
+            style={{
+              background: `${type.color}`,
+            }}
+          >
+            <h4>
+              Offensive properties of
+              <img src={type.svg} className="title-img" />
+              {type.type} type moves
+            </h4>
+          </div>
+          <h3>Super effective (×2): </h3>
+          <div id="effectiveness">
+            {type.effective.map((el, i) => (
+              <a
+                href={`#${el}`}
+                key={i}
+                className="effective"
+                style={{
+                  background: `${colorRef
+                    .filter((e) => e.name === el)
+                    .map((c) => c.color)}`,
+                }}
+              >
+                <img
+                  src={`https://github.com/duiker101/pokemon-type-svg-icons/blob/master/icons/${el.toLowerCase()}.svg?raw=true}`}
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    margin: "-23px -15px -23px -23px",
+                  }}
+                />
+                {el}
+              </a>
+            ))}
+          </div>
+
+          <h3>Not very effective (×½): </h3>
+          <div id="noteffectiveness">
+            {type.noteffective.map((el, i) => (
+              <a
+                href={`#${el}`}
+                key={i}
+                className="noteffective"
+                style={{
+                  background: `${colorRef
+                    .filter((e) => e.name === el)
+                    .map((c) => c.color)}`,
+                }}
+              >
+                <img
+                  src={`https://github.com/duiker101/pokemon-type-svg-icons/blob/master/icons/${el.toLowerCase()}.svg?raw=true}`}
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    margin: "-23px -15px -23px -23px",
+                  }}
+                />
+                {el}
+              </a>
+            ))}
+          </div>
+
+          <h3>Has no effect (x0): </h3>
+          <div id="noeffect">
+            {type.noeffect.map((el, i) => (
+              <a
+                href={`#${el}`}
+                key={i}
+                className="noeffectiveness"
+                style={{
+                  background: `${colorRef
+                    .filter((e) => e.name === el)
+                    .map((c) => c.color)}`,
+                }}
+              >
+                <img
+                  src={`https://github.com/duiker101/pokemon-type-svg-icons/blob/master/icons/${el.toLowerCase()}.svg?raw=true}`}
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    margin: "-23px -15px -23px -23px",
+                  }}
+                />
+                {el}
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div
+          className="defense-info"
+          style={{ border: `2px solid ${type.color}` }}
+        >
+          <div
+            className="info-border"
+            style={{
+              background: `${type.color}`,
+            }}
+          >
+            <h4>
+              Defensive properties of
+              <img src={type.svg} className="title-img" />
+              {type.type} type moves
+            </h4>
+          </div>
+          <h3>Weak to (x2): </h3>
+          <div id="weaknesses">
+            {type.weakness.map((el, i) => (
+              <a
+                href={`#${el}`}
+                key={i}
+                className="weakness"
+                style={{
+                  background: `${colorRef
+                    .filter((e) => e.name === el)
+                    .map((c) => c.color)}`,
+                }}
+              >
+                <img
+                  src={`https://github.com/duiker101/pokemon-type-svg-icons/blob/master/icons/${el.toLowerCase()}.svg?raw=true}`}
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    margin: "-23px -15px -23px -23px",
+                  }}
+                />
+                {el}
+              </a>
+            ))}
+          </div>
+
+          <h3>Resists (×½): </h3>
+          <div id="resistances">
+            {type.resist.map((el, i) => (
+              <a
+                href={`#${el}`}
+                key={i}
+                className="resistance"
+                style={{
+                  background: `${colorRef
+                    .filter((e) => e.name === el)
+                    .map((c) => c.color)}`,
+                }}
+              >
+                <img
+                  src={`https://github.com/duiker101/pokemon-type-svg-icons/blob/master/icons/${el.toLowerCase()}.svg?raw=true}`}
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    margin: "-23px -15px -23px -23px",
+                  }}
+                />
+                {el}
+              </a>
+            ))}
+          </div>
+
+          <h3>Immune to (x0): </h3>
+          <div id="immune">
+            {type.immune.map((el, i) => (
+              <a
+                href={`#${el}`}
+                key={i}
+                className="immunity"
+                style={{
+                  background: `${colorRef
+                    .filter((e) => e.name === el)
+                    .map((c) => c.color)}`,
+                }}
+              >
+                <img
+                  src={`https://github.com/duiker101/pokemon-type-svg-icons/blob/master/icons/${el.toLowerCase()}.svg?raw=true}`}
+                  style={{
+                    width: "15px",
+                    height: "15px",
+                    margin: "-23px -15px -23px -23px",
+                  }}
+                />
+                {el}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    ));
+  return (
+    <>
+      <div id="wrap">
+        <h1>Pokemon Types Chart</h1>
+        <div id="sig">- By R McGregor</div>
+        <input
+          id="input-search"
+          placeholder="Search for Pokemon Types here..."
+          onChange={handleChange}
+        ></input>
+        <div className="type-display-box">{displayTypes}</div>
+      </div>
+    </>
+  );
+}
---
+++ src/colors.tsx
@@ -0,0 +1,78 @@
+export const colorRef = [
+  {
+    name: "Normal",
+    color: "#A0A29F",
+  },
+  {
+    name: "Fire",
+    color: "#EE8130",
+  },
+  {
+    name: "Fighting",
+    color: "#C22E28",
+  },
+  {
+    name: "Water",
+    color: "#539DDF",
+  },
+  {
+    name: "Flying",
+    color: "#A1BBEC",
+  },
+  {
+    name: "Grass",
+    color: "#7AC74C",
+  },
+  {
+    name: "Poison",
+    color: "#B763CF",
+  },
+  {
+    name: "Electric",
+    color: "#F7D02C",
+  },
+  {
+    name: "Ground",
+    color: "#915121",
+  },
+  {
+    name: "Psychic",
+    color: "#F95587",
+  },
+  {
+    name: "Rock",
+    color: "#C9BB8A",
+  },
+  {
+    name: "Ice",
+    color: "#96D9D6",
+  },
+  {
+    name: "Bug",
+    color: "#92BC2C",
+  },
+  {
+    name: "Dragon",
+    color: "#0C69C8",
+  },
+  {
+    name: "Ghost",
+    color: "#5F6DBC",
+  },
+  {
+    name: "None",
+    color: "#ebe8e8",
+  },
+  {
+    name: "Steel",
+    color: "#5695A3",
+  },
+  {
+    name: "Fairy",
+    color: "#EE90E6",
+  },
+  {
+    name: "Dark",
+    color: "#595761",
+  },
+];
